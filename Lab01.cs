969c90ed2e84907449de4d8758ce194639caff9f
hanikeepers <hanikeepers@gmail.com>
2024-09-17 00:16:14 +0900

    브런치연습1
    브런치연습중

---
+++ Lab01.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnityGithubProject01
+{
+    public class Lab01
+    {
+        Debog.Log("hello world");
+    }
+}
